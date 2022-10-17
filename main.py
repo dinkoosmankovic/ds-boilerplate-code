f675e87c57c66c076b7af01d316f57505e6efd59
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2022-10-17 22:49:29 +0200

    boilerplate code finished

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import models as Mdl
 
 if __name__ == "__main__":
     box = Mdl.StupidBox()
+    # box.show_box()
+    box.integrate()
--- models/stupid_box.py
+++ models/stupid_box.py
@@ -4,11 +4,13 @@ import numpy as np
 import trimesh
 import trimesh.transformations as transforms
 import pyrender
+import time
 
 class StupidBox(Model):
     def __init__(self) -> None:
         Model.__init__(self)
         self.scene = None
+        self.state = 0
         self.__build_model()
         
     def __get_node_by_name(self, name):
@@ -52,13 +54,11 @@ class StupidBox(Model):
         
         box_1_matrix = self.__compose_transform_matrix(box_1_translation, box_1_rotation)
         
-        print(box_1_matrix)
-        
         self.scene.add_node(
             pyrender.Node(name="box_1", mesh=box_1_mesh, matrix=box_1_matrix)
         )
         
-        box_2 = trimesh.creation.box(extents=np.array([0.05, 0.15, 0.2]))
+        box_2 = trimesh.creation.box(extents=np.array([0.05, 0.05, 0.2]))
         box_2.visual.face_colors = np.array([1., 0, 0, 0.6])
         box_2_mesh = pyrender.Mesh.from_trimesh(box_2, smooth=False)
         
@@ -67,24 +67,53 @@ class StupidBox(Model):
         
         box_2_matrix = np.matmul(self.__get_node_by_name("box_1").matrix, self.__compose_transform_matrix(box_2_translation, box_2_rotation))
         
-        print(box_2_matrix)
-        
         self.scene.add_node(
             pyrender.Node(name="box_2", mesh=box_2_mesh, matrix=box_2_matrix)
         )
         
-        print(self.scene.nodes)
+        box_3 = trimesh.creation.box(extents=np.array([0.6, 0.05, 0.05]))
+        box_3.visual.face_colors = np.array([0, 0, 1., 0.6])
+        box_3_mesh = pyrender.Mesh.from_trimesh(box_3, smooth=False)
         
-        v = pyrender.Viewer(self.scene, use_raymond_lighting=True)
+        box_3_translation = transforms.translation_matrix(np.array([0, 0, 0.1 + 0.025]))
+        box_3_rotation = transforms.rotation_matrix(self.state, [0, 0, 1], box_3_mesh.centroid)
+        
+        box_3_matrix = np.matmul(self.__get_node_by_name("box_2").matrix, self.__compose_transform_matrix(box_3_translation, box_3_rotation))
+        
+        self.scene.add_node(
+            pyrender.Node(name="box_3", mesh=box_3_mesh, matrix=box_3_matrix)
+        )       
 
         
 
     def show_box(self):
-        self.scene.show()
+        v = pyrender.Viewer(self.scene, use_raymond_lighting=True)
 
+    def integrate(self, final_time=10, dt=0.1) -> None:
+        v = pyrender.Viewer(self.scene, run_in_thread=True,
+                            use_raymond_lighting=True)
+        t = 0
+        while v.is_active or t < final_time:
+            self.step(u=0.1, dt=dt)
+            v.render_lock.acquire()
+            self.__update_model()
+            v.render_lock.release()
+            t += dt
+            time.sleep(dt)
+            
 
-    def integrate(self) -> None:
-        return None
+    def step(self, u=0, dt=0.1):
+        self.state += u
+        
+    def __update_model(self):
+        origin = self.__get_node_by_name("box_3").mesh.centroid
+        box_3_translation = transforms.translation_matrix(np.array([0, 0, 0.1 + 0.025]))
+        box_3_rotation = transforms.rotation_matrix(self.state, [0, 0, 1], origin)
+        
+        box_3_matrix = np.matmul(self.__get_node_by_name("box_2").matrix, self.__compose_transform_matrix(box_3_translation, box_3_rotation))
+        self.__get_node_by_name("box_3").matrix = box_3_matrix
+        
+        
 
     def show_model(self) -> None:
         pass
